0f20d442d5ed2d740b32a067ff098d1720ca03d9
Stéphane Gully <stephane.gully@gmail.com>
2016-12-30 17:23:34 +0100

    rename demo-log-reader.sh -> demo-logs-reader.sh because it's simplier for shell commande...

--- demo-logs-injector.sh
+++ demo-logs-injector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Run 10 demo-log-reader.sh in parallel
-# On run example: ./demo-log-reader.sh ./demo-logs/inc.log
+# Run 10 demo-logs-reader.sh in parallel
+# On run example: ./demo-logs-reader.sh ./demo-logs/inc.log
 #
 
 if [ ! -f /usr/bin/parallel ]; then
@@ -12,4 +12,4 @@ ls -1 demo-logs/*.log | parallel \
   --no-notice \
   -j $(ls -1 demo-logs/*.log | wc -l) \
   --workdir $PWD \
-  ./demo-log-reader.sh {}
+  ./demo-logs-reader.sh {}
