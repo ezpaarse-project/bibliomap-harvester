f6e7b9920ac114b429e09718ed991d1be9fefaaf
Stéphane Gully <stephane.gully@gmail.com>
2017-01-31 19:19:18 +0100

    security: do not output demo logs in the root tmp/ folder because it could overlap production logs

--- demo-logs-reader.sh
+++ demo-logs-reader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # Read one log file line by line and 
-# write each line in desitnation tmp file.
+# write each line in destination tmp file.
 # This script is called by demo-logs-injector.sh
 # 
-# This destination file (ex ./tmp/inc.log) is watch by
+# This destination file (ex ./demo-logs/tmp/inc.log) is watch by
 # log.io-harvester in another process
 #
 
@@ -13,9 +13,9 @@ LOGRATE=0.2
 
 while true
 do
-  echo -n "" > ./tmp/$(basename $LOGFILE)
+  echo -n "" > ./demo-logs/tmp/$(basename $LOGFILE)
   while IFS='' read -r line || [[ -n "$line" ]]; do
-      echo $line >> ./tmp/$(basename $LOGFILE)
+      echo $line >> ./demo-logs/tmp/$(basename $LOGFILE)
       sleep $LOGRATE
   done < $LOGFILE
 done
